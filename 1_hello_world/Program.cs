0591888e60405d588a42bbbbe43c3797e8a6ae4f
김상윤 <sykim@miksystem.net>
2021-03-20 17:22:34 +0900

    project init

---
+++ 1_hello_world/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace _1_hello_world
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Send();
+            Thread.Sleep(2000);
+            Receive();
+        }
+
+        static void Receive()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "hello-queue",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($" [x] Received {message}");
+                };
+
+                channel.BasicConsume(
+                    queue: "hello-queue",
+                    autoAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+
+        static void Send()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "hello-queue",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var message = "Hello World using RabbitMQ!";
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(
+                    exchange: "",
+                    routingKey: "hello-queue",
+                    basicProperties: null,
+                    body: body);
+
+                Console.WriteLine($" [x] Sent {message}");
+            }
+
+            Console.WriteLine(" Press [enter] to send.");
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # rabbitmq-tutorial-cs
-rabbitmq tutorial in .net 5.0
+This is rabbitmq tutorial in .net 5.0
+
+
+
+### Requirements
+
+.net 5.0
+
+
+
+### How to Build
+
+If you haven't installed Chocolatey, install it first.
+
+[Installing Chocolatey](https://chocolatey.org/install)
+
+```powershell
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+```
+
+![img](images\clip_image001.jpg)
+
+Then, install RabbitMQ in Powershell.
+
+[Installing on Windows — RabbitMQ](https://www.rabbitmq.com/install-windows.html)
+
+```powershell
+choco install rabbitmq
+```
+
+![image-20210320171640059](images\clip_image002.jpg)
+
+
+
+Then you can run the solution file and then debug it can be debugged.
+
+![image-20210320171945881](images\clip_image003.jpg)
